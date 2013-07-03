53e5db435f3fa7e64bfaea7845ea5af2285437fc
Jason Waters <waters@attask.com>
2013-07-03 17:15:29 -0600

    got rid of flicker when countdown starts up.

--- js/countdown-clock.js
+++ js/countdown-clock.js
@@ -7,15 +7,40 @@
     var SECONDS_PER_HOUR = SECONDS_PER_MINUTE * MINUTES_PER_HOUR;
     var SECONDS_PER_DAY = SECONDS_PER_MINUTE * MINUTES_PER_HOUR * HOURS_PER_DAY;
 
+    function getTimeLeft(targetDate) {
+        var days = 0, hours = 0, minutes = 0, secs = 0;
+
+        var now = new Date();
+        var diff = (targetDate.getTime() - now.getTime() + 5)/MILLIS_PER_SECOND; //in seconds
+
+        if (diff >= 0) {
+            days = Math.floor(diff / SECONDS_PER_DAY);
+            diff = diff % SECONDS_PER_DAY; //remaining seconds in excess of full days
+
+            hours = Math.floor(diff / SECONDS_PER_HOUR);
+            diff = diff % SECONDS_PER_HOUR; //remaining seconds in excess of full hours
+
+            minutes = Math.floor(diff / SECONDS_PER_MINUTE);
+            diff = diff % SECONDS_PER_MINUTE; //remaining seconds in excess of full minutes
+
+            secs = Math.floor(diff);
+        }
+
+        return {
+            'diff': diff,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': secs
+        };
+    }
+
     var countdownApp = angular.module('countdown', []);
 
     countdownApp.value('targetDate', moment(COUNTDOWN_TARGET_DATE).toDate());
 
     countdownApp.filter('pad', function() {
         return function (value, size) {
-            if(value < 0) {
-                return "  ";
-            }
             var s = value+"";
             while (s.length < size) s = "0" + s;
             return s;
@@ -24,42 +49,18 @@
 
     countdownApp.controller('CountdownController', function($scope, targetDate) {
         var timer = setInterval(function() {
-            var days = 0, hours = 0, mins = 0, secs = 0;
+            var timeLeft = getTimeLeft(targetDate);
 
-            var now = new Date();
-            var diff = (targetDate.getTime() - now.getTime() + 5)/MILLIS_PER_SECOND; //in seconds
-
-            if (diff < 0) {
+            if (timeLeft.diff < 0) {
                 clearInterval(timer);
                 timer = null;
-            } else {
-                days = Math.floor(diff / SECONDS_PER_DAY);
-                diff = diff % SECONDS_PER_DAY; //remaining seconds in excess of full days
-
-                hours = Math.floor(diff / SECONDS_PER_HOUR);
-                diff = diff % SECONDS_PER_HOUR; //remaining seconds in excess of full hours
-
-                mins = Math.floor(diff / SECONDS_PER_MINUTE);
-                diff = diff % SECONDS_PER_MINUTE; //remaining seconds in excess of full minutes
-
-                secs = Math.floor(diff);
             }
 
             $scope.$apply(function() {
-               $scope.data = {
-                   'days': days,
-                   'hours': hours,
-                   'minutes': mins,
-                   'seconds': secs
-               };
+               $scope.data = timeLeft;
             });
         }, 1000);
 
-        $scope.data = {
-            'days': -1,
-            'hours': -1,
-            'minutes': -1,
-            'seconds': -1
-        };
+        $scope.data = getTimeLeft(targetDate);
     });
 })();
